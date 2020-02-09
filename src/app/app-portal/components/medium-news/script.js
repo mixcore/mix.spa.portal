3c78a546c0648bed37e7862e0590c4ff26c73e84
Smilefounder <i.love.to.smile.around@gmail.com>
2020-02-09 14:14:50 +0700

    upd

--- src/app/app-portal/components/medium-news/script.js
+++ src/app/app-portal/components/medium-news/script.js
@@ -6,7 +6,7 @@
       var ctrl = this;
       ctrl.items = [];
       ctrl.init = function () {
-        var req= {
+        var req = {
           method: 'GET',
           url: 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/mixcore?t=' + Math.floor(Date.now() / 1000)
         };
@@ -15,13 +15,12 @@
 
       ctrl.getMediumApiResult = async function (req) {
         return $http(req).then(function (resp) {
-          if(resp.status == '200')
-          {
-              ctrl.items = resp.data.items;
+          if (resp.status == '200') {
+            ctrl.items = resp.data.items;
           }
-          else{
+          else {
             console.log(resp);
- 
+
           }
         },
           function (error) {
