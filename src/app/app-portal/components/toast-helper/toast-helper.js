400ece8f27cb04eb996d6d02d875b6adf29c8af6
Smilefounder <i.love.to.smile.around@gmail.com>
2021-01-16 21:17:00 +0700

    upd styles

---
+++ src/app/app-portal/components/toast-helper/toast-helper.js
@@ -0,0 +1,14 @@
+﻿modules.component("toastHelper", {
+  templateUrl: "/mix-app/views/app-portal/components/toast-helper/toast-helper.html",
+  bindings: {
+    url: "=?",
+    title: "=?",
+  },
+  controller: [
+    "$rootScope",
+    function ($rootScope) {
+      var ctrl = this;      
+      ctrl.visible= $rootScope.visible;
+    },
+  ],
+});
