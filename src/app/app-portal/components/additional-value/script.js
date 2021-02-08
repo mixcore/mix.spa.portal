e99dd36dd5fa5a4ae406c6a40c1642bdb8983bbf
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-02-08 08:39:56 +0700

    fix additional data

--- src/app/app-portal/components/additional-value/script.js
+++ src/app/app-portal/components/additional-value/script.js
@@ -43,49 +43,6 @@ modules.component("additionalValue", {
           }
         }
       };
-
-      ctrl.updateOrders = function (index) {
-        if (index > ctrl.dragStartIndex) {
-          ctrl.fields.splice(ctrl.dragStartIndex, 1);
-        } else {
-          ctrl.fields.splice(ctrl.dragStartIndex + 1, 1);
-        }
-        angular.forEach(ctrl.fields, function (e, i) {
-          e.priority = i;
-        });
-      };
-
-      ctrl.dragStart = function (index) {
-        ctrl.dragStartIndex = index;
-      };
-
-      ctrl.removeAttribute = function (val, index) {
-        $rootScope.showConfirm(
-          ctrl,
-          "removeAttributeConfirmed",
-          [val, index],
-          null,
-          "Remove Field",
-          "Deleted data will not able to recover, are you sure you want to delete this item?"
-        );
-      };
-      ctrl.removeAttributeConfirmed = async function (val, index) {
-        if (val.id) {
-          $rootScope.isBusy = true;
-          var result = await valueService.delete([val.id]);
-          if (result.isSucceed) {
-            ctrl.model.additionalData.data.values.splice(index, 1);
-            $rootScope.isBusy = false;
-            $scope.$apply();
-          } else {
-            $rootScope.showErrors(result.errors);
-            $rootScope.isBusy = false;
-            $scope.$apply();
-          }
-        } else {
-          ctrl.model.additionalData.data.values.splice(index, 1);
-        }
-      };
     },
   ],
 });
