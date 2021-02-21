2b91dcafc617e5a3d738d23bb9a252cb9369a78f
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-02-22 00:19:38 +0700

    fix store portal

--- src/app/app-portal/pages/store/store-controller.js
+++ src/app/app-portal/pages/store/store-controller.js
@@ -2,7 +2,15 @@
 app.controller('StoreController',
     ['$scope', '$rootScope', 'ngAppSettings', '$routeParams', '$location', 'StoreService',
         function ($scope, $rootScope, ngAppSettings, $routeParams, $location, service) {
-            BaseCtrl.call(this, $scope, $rootScope, $routeParams, ngAppSettings, service);
+            BaseRestCtrl.call(
+                this,
+                $scope,
+                $rootScope,
+                $location,
+                $routeParams,
+                ngAppSettings,
+                service
+              );
             $scope.request.key = 'service.store';
             $scope.cates = ngAppSettings.enums.configuration_cates;
             $scope.settings = $rootScope.globalSettings;
