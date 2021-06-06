975be396afb85c72e74d1bcd7c34a5ff455e275f
Huy Nguyen <Smilefounder@users.noreply.github.com>
2021-06-06 16:58:53 +0700

    upd init UI

--- src/app/app-shared/components/pills/pane.js
+++ src/app/app-shared/components/pills/pane.js
@@ -4,7 +4,7 @@ sharedComponents.directive("pill", function () {
     require: "^pills",
     restrict: "E",
     transclude: true,
-    scope: { header: "@", id: "@" },
+    scope: { header: "@", id: "@", icon: "@" },
     link: function (scope, element, attrs, tabsController) {
       tabsController.addPane(scope);
     },
