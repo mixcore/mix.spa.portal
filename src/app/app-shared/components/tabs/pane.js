f9baf1000bdf46fd9cd264fa78dee7eae35856a3
Restyled.io <commits@restyled.io>
2021-06-19 15:38:12 +0000

    Restyled by clang-format

--- src/app/app-shared/components/tabs/pane.js
+++ src/app/app-shared/components/tabs/pane.js
@@ -1,16 +1,15 @@
 ﻿"use trick";
-sharedComponents.directive("pane", function () {
+sharedComponents.directive("pane", function() {
   return {
-    require: "^tabs",
-    restrict: "E",
-    transclude: true,
-    scope: { header: "@", id: "@", icon: "@" },
-    link: function (scope, element, attrs, tabsController) {
-      tabsController.addPane(scope);
-    },
-    template:
-      '<div class="tab-pane" ng-class="{active: selected}" ng-transclude>' +
-      "</div>",
-    replace: true,
+    require : "^tabs",
+    restrict : "E",
+    transclude : true,
+    scope : {header : "@", id : "@", icon : "@"},
+    link : function(scope, element, attrs,
+                    tabsController) { tabsController.addPane(scope); },
+    template :
+        '<div class="tab-pane" ng-class="{active: selected}" ng-transclude>' +
+            "</div>",
+    replace : true,
   };
 });
