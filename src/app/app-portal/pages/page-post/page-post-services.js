7b33cba2985856f2f0f4f65ffbff1f3b86e1f17c
Huy Nguyen <Smilefounder@users.noreply.github.com>
2021-09-08 16:51:38 +0700

    fix portal > module detail > /module-data/list url

--- src/app/app-portal/pages/page-post/page-post-services.js
+++ src/app/app-portal/pages/page-post/page-post-services.js
@@ -1,30 +1,30 @@
 ﻿"use strict";
 app.factory("PagePostRestService", [
-  "$rootScope",
-  "ApiService",
-  "CommonService",
-  "BaseService",
-  function ($rootScope, apiService, commonService, baseService) {
-    var serviceFactory = Object.create(baseService);
-    serviceFactory.init("page-post");
-    var _delete = async function (id) {
-      var url = this.prefixUrl + "/delete/" + id;
-      var req = {
-        method: "GET",
-        url: url,
-      };
-      return await apiService.getApiResult(req);
-    };
-    var _updateInfos = async function (pages) {
-      var req = {
-        method: "POST",
-        url: this.prefixUrl + "/update-infos",
-        data: JSON.stringify(pages),
-      };
-      return await apiService.getApiResult(req);
-    };
-    serviceFactory.delete = _delete;
-    serviceFactory.updateInfos = _updateInfos;
-    return serviceFactory;
-  },
+    "$rootScope",
+    "ApiService",
+    "CommonService",
+    "BaseService",
+    function($rootScope, apiService, commonService, baseService) {
+        var serviceFactory = Object.create(baseService);
+        serviceFactory.init("page-post");
+        var _delete = async function(id) {
+            var url = this.prefixUrl + "/delete/" + id;
+            var req = {
+                method: "GET",
+                url: url,
+            };
+            return await apiService.getApiResult(req);
+        };
+        var _updateInfos = async function(pages) {
+            var req = {
+                method: "POST",
+                url: this.prefixUrl + "/update-infos",
+                data: JSON.stringify(pages),
+            };
+            return await apiService.getApiResult(req);
+        };
+        serviceFactory.delete = _delete;
+        serviceFactory.updateInfos = _updateInfos;
+        return serviceFactory;
+    },
 ]);
