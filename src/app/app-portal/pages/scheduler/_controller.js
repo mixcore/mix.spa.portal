8eb2202d9fe75720cc3692a1ed37075f10e7b95f
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2022-04-07 22:29:28 +0700

    add scheduler portal

---
+++ src/app/app-portal/pages/scheduler/_controller.js
@@ -0,0 +1,138 @@
+﻿"use strict";
+app.controller("SchedulerController", [
+  "$scope",
+  "$rootScope",
+  "$routeParams",
+  "$location",
+  "SchedulerService",
+  function ($scope, $rootScope, $routeParams, $location, service) {
+    $scope.jobs = [];
+    $scope.intervalTypes = ["Second", "Minute", "Hour", "Day", "Month", "Year"];
+    $scope.schedule = {
+      jobData: {},
+      cronExpression: null,
+      name: null,
+      groupName: null,
+      jobName: null,
+      description: null,
+      startAt: null,
+      isStartNow: false,
+      interval: null,
+      intervalType: null,
+      repeatCount: null,
+    };
+    $scope.init = function () {
+      $scope.getTriggers();
+      $scope.getJobs();
+    };
+
+    $scope.getSingle = async function () {
+      $scope.getJobs();
+      $scope.getTrigger();
+    };
+
+    $scope.getTrigger = async function () {
+      if ($routeParams.name) {
+        $rootScope.isBusy = true;
+        var resp = await service.getTrigger($routeParams.name);
+        if (resp && resp.success) {
+          $scope.trigger = resp.data;
+          $scope.schedule.jobName = $scope.trigger.jobName;
+          $scope.schedule.groupName = $scope.trigger.group;
+          $scope.schedule.interval = $scope.trigger.repeatInterval;
+          $scope.schedule.repeatCount = $scope.trigger.repeatCount;
+          $rootScope.isBusy = false;
+          $scope.$apply();
+        } else {
+          if (resp) {
+            $rootScope.showErrors(resp.errors || ["Failed"]);
+          }
+          $rootScope.isBusy = false;
+          $scope.$apply();
+        }
+      }
+    };
+
+    $scope.createSchedule = async function () {
+      $rootScope.isBusy = true;
+      var resp = await service.createSchedule($scope.schedule);
+      if (resp && resp.success) {
+        $rootScope.isBusy = true;
+        $location.url("/portal/scheduler");
+        $scope.$apply();
+      } else {
+        if (resp) {
+          $rootScope.showErrors(resp.errors || ["Failed"]);
+        }
+        $rootScope.isBusy = false;
+        $scope.$apply();
+      }
+    };
+    $scope.updateJobData = function (content) {
+      try {
+        $scope.schedule.jobData = JSON.parse(content);
+        $scope.$apply();
+      } catch {}
+    };
+    $scope.pauseTrigger = async function (name) {
+      $rootScope.isBusy = true;
+      var resp = await service.pauseTrigger(name);
+      if (resp && resp.success) {
+        $scope.schedule = resp.data;
+        await $scope.getTriggers();
+      } else {
+        if (resp) {
+          $rootScope.showErrors(resp.errors || ["Failed"]);
+        }
+        $rootScope.isBusy = false;
+        $scope.$apply();
+      }
+    };
+
+    $scope.resumeTrigger = async function (name) {
+      $rootScope.isBusy = true;
+      var resp = await service.resumeTrigger(name);
+      if (resp && resp.success) {
+        $scope.schedule = resp.data;
+        await $scope.getTriggers();
+      } else {
+        if (resp) {
+          $rootScope.showErrors(resp.errors || ["Failed"]);
+        }
+        $rootScope.isBusy = false;
+        $scope.$apply();
+      }
+    };
+
+    $scope.getTriggers = async function () {
+      $rootScope.isBusy = true;
+      var resp = await service.getTriggers();
+      if (resp && resp.success) {
+        $scope.data = resp.data;
+        $rootScope.isBusy = false;
+        $scope.$apply();
+      } else {
+        if (resp) {
+          $rootScope.showErrors(resp.errors || ["Failed"]);
+        }
+        $rootScope.isBusy = false;
+        $scope.$apply();
+      }
+    };
+    $scope.getJobs = async function () {
+      $rootScope.isBusy = true;
+      var resp = await service.getJobs();
+      if (resp && resp.success) {
+        $scope.jobs = resp.data;
+        $rootScope.isBusy = false;
+        $scope.$apply();
+      } else {
+        if (resp) {
+          $rootScope.showErrors(resp.errors || ["Failed"]);
+        }
+        $rootScope.isBusy = false;
+        $scope.$apply();
+      }
+    };
+  },
+]);
---
+++ src/app/app-portal/pages/scheduler/_services.js
@@ -0,0 +1,114 @@
+﻿"use strict";
+app.factory("SchedulerService", [
+  "AuthService",
+  "ApiService",
+  function (authService, apiService) {
+    var serviceFactory = {};
+    serviceFactory.prefixUrl = "/scheduler";
+    var _createSchedule = async function (schedule) {
+      var url = `${this.prefixUrl}/trigger/create`;
+      var req = {
+        serviceBase: this.serviceBase,
+        method: "POST",
+        url: url,
+        data: JSON.stringify(schedule),
+      };
+      return await this.getRestApiResult(req);
+    };
+    var _pauseTrigger = async function (name) {
+      var url = `${this.prefixUrl}/trigger/pause/${name}`;
+      var req = {
+        serviceBase: this.serviceBase,
+        method: "GET",
+        url: url,
+      };
+      return await this.getRestApiResult(req);
+    };
+
+    var _resumeTrigger = async function (name) {
+      var url = `${this.prefixUrl}/trigger/resume/${name}`;
+      var req = {
+        serviceBase: this.serviceBase,
+        method: "GET",
+        url: url,
+      };
+      return await this.getRestApiResult(req);
+    };
+
+    var _getTrigger = async function (name) {
+      var url = `${this.prefixUrl}/trigger/${name}`;
+      var req = {
+        serviceBase: this.serviceBase,
+        method: "GET",
+        url: url,
+      };
+      return await this.getRestApiResult(req);
+    };
+
+    var _getTriggers = async function () {
+      var url = `${this.prefixUrl}/trigger`;
+      var req = {
+        serviceBase: this.serviceBase,
+        method: "GET",
+        url: url,
+      };
+      return await this.getRestApiResult(req);
+    };
+    var _getJobs = async function () {
+      var url = `${this.prefixUrl}/job`;
+      var req = {
+        serviceBase: this.serviceBase,
+        method: "GET",
+        url: url,
+      };
+      return await this.getRestApiResult(req);
+    };
+
+    var _getRestApiResult = async function (req) {
+      if (!authService.authentication) {
+        await authService.fillAuthData();
+      }
+      if (authService.authentication) {
+        req.Authorization = authService.authentication.accessToken;
+      }
+      if (!req.headers) {
+        req.headers = {
+          "Content-Type": "application/json",
+        };
+      }
+      req.headers.Authorization = "Bearer " + req.Authorization || "";
+
+      return apiService.sendRequest(req).then(function (resp) {
+        return resp;
+      });
+    };
+    var _parseQuery = function (req) {
+      var result = "";
+      if (req) {
+        for (var key in req) {
+          if (angular.isObject(req.query)) {
+            req.query = JSON.stringify(req.query);
+          }
+          if (req.hasOwnProperty(key) && req[key]) {
+            if (result != "") {
+              result += "&";
+            }
+            result += `${key}=${req[key]}`;
+          }
+        }
+        return result;
+      } else {
+        return result;
+      }
+    };
+    serviceFactory.parseQuery = _parseQuery;
+    serviceFactory.getRestApiResult = _getRestApiResult;
+    serviceFactory.createSchedule = _createSchedule;
+    serviceFactory.getJobs = _getJobs;
+    serviceFactory.getTrigger = _getTrigger;
+    serviceFactory.getTriggers = _getTriggers;
+    serviceFactory.pauseTrigger = _pauseTrigger;
+    serviceFactory.resumeTrigger = _resumeTrigger;
+    return serviceFactory;
+  },
+]);
