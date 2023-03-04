01921aee2325093cf164f60c85833078035ec461
Eric Nguyen <nhathoang989@gmail.com>
2023-03-04 11:23:28 +0700

    mix - add audit log

---
+++ src/app/app-portal/pages/audit-log/controller.js
@@ -0,0 +1,40 @@
+﻿"use strict";
+app.controller("AuditLogController", [
+  "$scope",
+  "$rootScope",
+  "ngAppSettings",
+  "$location",
+  "$routeParams",
+  "AuditLogRestService",
+  function (
+    $scope,
+    $rootScope,
+    ngAppSettings,
+    $location,
+    $routeParams,
+    service
+  ) {
+    BaseRestCtrl.call(
+      this,
+      $scope,
+      $rootScope,
+      $location,
+      $routeParams,
+      ngAppSettings,
+      service
+    );
+    // $scope.request.columns = [
+    //   "id",
+    //   "displayName",
+    //   "host",
+    //   "createdDateTime",
+    //   "createdBy",
+    // ];
+    $scope.canDrag =
+      $scope.request.orderBy !== "Priority" || $scope.request.direction !== "0";
+
+    $scope.view = function (item) {
+      $rootScope.preview("object", item, null, "modal-lg");
+    };
+  },
+]);
---
+++ src/app/app-portal/pages/audit-log/services.js
@@ -0,0 +1,9 @@
+﻿"use strict";
+app.factory("AuditLogRestService", [
+  "BaseRestService",
+  function (baseService) {
+    var serviceFactory = Object.create(baseService);
+    serviceFactory.init("audit-log");
+    return serviceFactory;
+  },
+]);
